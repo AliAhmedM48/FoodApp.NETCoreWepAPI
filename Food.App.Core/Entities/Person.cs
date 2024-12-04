a6f650f1d228d5340f1179926d9c078a9eb4c56c
Ali Ahmed <63220520+AliAhmedM48@users.noreply.github.com>
2024-12-04 12:31:38 +0200

    Add user model, user view model, and user details view model. Configure user mapping profiles.

---
+++ Food.App.Core/Entities/Person.cs
@@ -0,0 +1,10 @@
+﻿using Food.App.Core.Enums;
+
+namespace Food.App.Core.Entities;
+public class Person : BaseEntity
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public Role Role { get; set; }
+}
