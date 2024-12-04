3edfcfc125bfbcb88fe2abe3275c8d2325d5a74c
Ali Ahmed <63220520+AliAhmedM48@users.noreply.github.com>
2024-12-04 13:34:25 +0200

    Implement user service including the following operations: GetAll, GetOne, and Delete.

---
+++ Food.App.Core/Interfaces/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Food.App.Core.ViewModels;
+
+namespace Food.App.Core.Interfaces.Services;
+public interface IUserService
+{
+    IEnumerable<UserViewModel> GetAllUsers();
+    UserDetailsViewModel GetUserDetailsById(int id);
+    Task DeleteUserByIdAsync(int id);
+    //TODO: Implement method GetAllFavoriteRecipesByUserId
+    //IEnumerable<RecipeViewModel> GetAllFavoriteRecipesByUserId(int id);
+}
