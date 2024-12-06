38f856a0b7fe39a553c39f090325078d20205e40
Mohamed Shokrani <104221047+mohamed-shokrani@users.noreply.github.com>
2024-12-07 00:00:54 +0200

    Add recipe  Delete Endpoint

--- Food.App.Core/ViewModels/Recipe/Update/UpdateRecipeViewModel.cs
+++ Food.App.Core/ViewModels/Recipe/Update/UpdateRecipeViewModel.cs
@@ -1,9 +1,9 @@
 ﻿namespace Food.App.Core.ViewModels.Recipe.Create;
 public class UpdateRecipeViewModel
 {
+    public int RecipeId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public string ImagePath { get; set; }
     public int CategoryId { get; set; }
     public List<int> TagIds { get; set; }
 
