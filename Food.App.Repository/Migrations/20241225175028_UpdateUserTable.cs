0b1df484cc2deff049e52dabf199d459f2450366
Mohamed Shokrani <104221047+mohamed-shokrani@users.noreply.github.com>
2024-12-25 19:51:00 +0200

    update User Table

---
+++ Food.App.Repository/Migrations/20241225175028_UpdateUserTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Food.App.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetCode",
+                table: "Users",
+                type: "nvarchar(6)",
+                maxLength: 6,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordResetCodeExpiration",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetCodeExpiration",
+                table: "Users");
+        }
+    }
+}
